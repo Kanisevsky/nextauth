8b0d894629a0e81e717bb51a5ae1417ad9c5f1ff
Kanisevsky <meltsmokestudio@gmail.com>
2024-12-15 19:56:09 +0000

    social buttons for login form

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,9 +1,20 @@
 'use client';
 
-import { Google } from 'lucide-react';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+import { Button } from '../ui/button';
 
 const Social = () => {
-  return <div className="flex items-center w-full gap-x-2"></div>;
+  return (
+    <div className="flex items-center w-full gap-x-2">
+      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+        <FcGoogle className="w-5 h-5" />
+      </Button>
+      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+        <FaGithub className="w-5 h-5" />
+      </Button>
+    </div>
+  );
 };
 
 export default Social;
